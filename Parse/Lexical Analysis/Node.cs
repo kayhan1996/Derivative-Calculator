beff369b15f0ad70a49f2093cfd8dd2b36a9a7e4
Kayhan Baig <kayhan1996@hotmail.com>
2017-08-21 21:19:09 -0600

    Moved the node operations into a seperate class to increase readability

--- Parse/Lexical Analysis/Node.cs
+++ Parse/Lexical Analysis/Node.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Parse {
-    public class Node : IEnumerable<Node> {
+    public partial class Node : IEnumerable<Node> {
         public String payload;
         public Types type;
         public Node leftChild;
@@ -98,50 +98,7 @@ namespace Parse {
             }
         }
 
-        public static Node operator *(Node x1, Node x2){
-            Node n = new Node("*", Types.Factor);
-            n.addLeftChild(x1);
-            n.addRightChild(x2);
-            return n;
-        }
-        public static Node operator ^(Node x1, Node x2) {
-            Node n = new Node("^", Types.Exponent);
-            n.addLeftChild(x1);
-            n.addRightChild(x2);
-            return n;
-        }
-        public static Node operator +(Node x1, Node x2) {
-            Node n = new Node("+", Types.Statement);
-            n.addLeftChild(x1);
-            n.addRightChild(x2);
-            return n;
-        }
-        public static Node operator /(Node x1, Node x2) {
-            Node n = new Node("/", Types.Factor);
-            n.addLeftChild(x1);
-            n.addRightChild(x2);
-            return n;
-        }
-        public static Node operator -(Node x1, Node x2) {
-            Node n = new Node("-", Types.Statement);
-            n.addLeftChild(x1);
-            n.addRightChild(x2);
-            return n;
-        }
-        public static Node operator ^(Node x1, double x2) {
-            Node n = new Node("-", Types.Exponent);
-            n.addLeftChild(x1);
-            n.addRightChild(new Node(x2.ToString(), Types.Exponent));
-            return n;
-        }
-        public static Node operator -(Node x1, double x2) {
-            if (x1.type == Types.Number) {
-                double x = double.Parse(x1.payload) - x2;
-                Node n = new Node(x.ToString(), Types.Number);
-                return n;
-            }
-            throw null;
-        }
+        
 
         public override string ToString() {
             return getTree(this);
---
+++ Parse/Lexical Analysis/NodeOperators.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse
+{
+    public partial class Node
+    {
+        public static Node operator *(Node x1, Node x2){
+            Node n = new Node("*", Types.Factor);
+            n.addLeftChild(x1);
+            n.addRightChild(x2);
+            return n;
+        }
+        public static Node operator ^(Node x1, Node x2) {
+            Node n = new Node("^", Types.Exponent);
+            n.addLeftChild(x1);
+            n.addRightChild(x2);
+            return n;
+        }
+        public static Node operator +(Node x1, Node x2) {
+            Node n = new Node("+", Types.Statement);
+            n.addLeftChild(x1);
+            n.addRightChild(x2);
+            return n;
+        }
+        public static Node operator /(Node x1, Node x2) {
+            Node n = new Node("/", Types.Factor);
+            n.addLeftChild(x1);
+            n.addRightChild(x2);
+            return n;
+        }
+        public static Node operator -(Node x1, Node x2) {
+            Node n = new Node("-", Types.Statement);
+            n.addLeftChild(x1);
+            n.addRightChild(x2);
+            return n;
+        }
+        public static Node operator ^(Node x1, double x2) {
+            Node n = new Node("-", Types.Exponent);
+            n.addLeftChild(x1);
+            n.addRightChild(new Node(x2.ToString(), Types.Exponent));
+            return n;
+        }
+        public static Node operator -(Node x1, double x2) {
+            if (x1.type == Types.Number) {
+                double x = double.Parse(x1.payload) - x2;
+                Node n = new Node(x.ToString(), Types.Number);
+                return n;
+            }
+            throw null;
+        }
+    }
+}
