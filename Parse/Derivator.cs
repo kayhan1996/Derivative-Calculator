ce7d787aaa4471fb628eaed094ae3fa36f599b50
Kayhan Baig <kayhan1996@gmail.com>
2017-08-22 04:18:42 -0600

    Organized Node partial classes to seperate folder

--- Parse/Derivator.cs
+++ Parse/Derivator.cs
@@ -8,26 +8,37 @@ namespace Parse {
     public class Derivator {
         public static Node dydx(Node n) {
             Node derivative = new Node("0", Attributes.Number);
-            if(n.Payload == "^") {
-                if(n.LeftChild.Attribute == Attributes.Variable && n.RightChild.Attribute == Attributes.Number) {
+
+            if (n.Payload == "^") {
+
+                if (n.Attribute == Attributes.Polynomial) {
                     derivative = (n.RightChild) * (n.LeftChild ^ (n.RightChild - 1));
                 }
-            }else if(n.Payload == "*") {
+
+            } else if (n.Payload == "*") {
+
                 derivative = (dydx(n.LeftChild) * (n.RightChild)) + (dydx(n.RightChild) * (n.LeftChild));
                 derivative.LeftChild.Attribute = Attributes.Parenthesized;
                 derivative.RightChild.Attribute = Attributes.Parenthesized;
-            }else if(n.Payload == "+") {
+
+            } else if (n.Payload == "+") {
+
                 derivative = dydx(n.LeftChild) + dydx(n.RightChild);
                 derivative.Attribute = Attributes.Parenthesized;
-            } else if(n.Payload == "/") {
+
+            } else if (n.Payload == "/") {
+
                 derivative = ((n.RightChild * dydx(n.LeftChild)) - (n.LeftChild * dydx(n.RightChild))) / (n.RightChild ^ 2);
-            }else if(n.Attribute == Attributes.Variable) {
+
+            } else if (n.Attribute == Attributes.Variable) {
+
                 derivative = new Node("1", Attributes.Number);
-            }else if(n.Attribute == Attributes.Number) {
+
+            } else if (n.Attribute == Attributes.Number) {
                 //Do nothing as the node is already set to "0"
             }
+
             return derivative;
-            
         }
     }
 }
